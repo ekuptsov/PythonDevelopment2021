327340c6d2d00bc59e5627c5033ffbf48aefd0b2
ekuptsov <s02150138@stud.cs.msu.ru>
2021-03-16 10:19:37 +0300

    add buttons movement, endgame message

--- 03_ThreeWayAndTkinter/MysticSquare.py
+++ 03_ThreeWayAndTkinter/MysticSquare.py
@@ -1,6 +1,9 @@
 import tkinter as tk
+from tkinter import messagebox
 import random
 import itertools
+from functools import partial
+from math import ceil
 
 ROWS = 4
 COLUMNS = 4
@@ -10,17 +13,48 @@ class MysticSquare(tk.Frame):
 
     def __init__(self, master=None):
         super().__init__(master)
-        self.grid(sticky=tk.N+tk.E+tk.W+tk.S)
+        self.grid(sticky='nswe')
         self.createWidgets()
 
     def shuffle(self):
         cols, rows = self.grid_size()
-        sticky = tk.N+tk.E+tk.W+tk.S
-        indexies = list(itertools.product(range(1, rows), range(cols)))
-        random.shuffle(indexies)
+        self.order = list(itertools.product(range(1, rows), range(cols)))
+        random.shuffle(self.order)
+        del self.order[-1]  # now len(order) == len(table)
 
-        for button, (row, column) in zip(self.table, indexies):
-            button.grid(row=row, column=column, sticky=sticky)
+        for button, (row, column) in zip(self.table, self.order):
+            button.grid(row=row, column=column, sticky='nswe')
+
+    def player_win(self):
+        cols, rows = self.grid_size()
+        win_order = itertools.product(range(1, rows), range(cols))
+        decision = all(curr == win for curr, win in zip(self.order, win_order))
+        return decision
+
+    def in_bounds(self, i, j):
+        cols, rows = self.grid_size()
+        return 1 <= i < rows and 0 <= j < cols
+
+    def move(self, bt_number):
+        idx = bt_number - 1
+        button = self.table[idx]
+        info = button.grid_info()
+        # print(info)
+        i, j = info['row'], info['column']
+        neighs = ((i-1, j), (i+1, j), (i, j-1), (i, j+1))
+        any_empty = [
+            (si, sj)
+            for si, sj in neighs
+            if self.in_bounds(si, sj) and
+               not self.grid_slaves(row=si, column=sj)
+        ]
+        if any_empty:
+            si, sj = any_empty.pop()
+            button.grid(row=si, column=sj, sticky='nswe')
+            self.order[idx] = (si, sj)
+            if self.player_win():
+                messagebox.showinfo(title='Win', message='Congratulations!')
+                self.shuffle()
 
     def createWidgets(self):
         top = self.winfo_toplevel()
@@ -29,8 +63,8 @@ class MysticSquare(tk.Frame):
 
         self.new_bt = tk.Button(self, text='New', command=self.shuffle)
         self.quit_bt = tk.Button(self, text='Quit', command=self.quit)
-        self.new_bt.grid(row=0, column=0, columnspan=COLUMNS // 2)
-        self.quit_bt.grid(row=0, column=COLUMNS // 2, columnspan=COLUMNS // 2)
+        self.new_bt.grid(row=0, column=0, columnspan=ceil(COLUMNS/2))
+        self.quit_bt.grid(row=0, column=COLUMNS // 2, columnspan=ceil(COLUMNS/2))
 
         for i in range(1, ROWS+1):
             self.rowconfigure(i, weight=1, minsize=80)
@@ -38,10 +72,12 @@ class MysticSquare(tk.Frame):
         for j in range(COLUMNS):
             self.columnconfigure(j, weight=1, minsize=80)
 
-        self.table = [
-            tk.Button(self, text=f'{i}')
-            for i in range(1, ROWS*COLUMNS)
-        ]
+        self.table = []
+        for i in range(1, ROWS*COLUMNS):
+            bt = tk.Button(self, text=str(i),
+                           command=partial(self.move, i),
+                           highlightcolor='white')
+            self.table.append(bt)
         self.shuffle()
 
 
