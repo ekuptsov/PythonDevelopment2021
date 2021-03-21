758676275c13a10c96d30d7780b153196e6cebae
ekuptsov <s02150138@stud.cs.msu.ru>
2021-03-21 10:02:18 +0300

    skeleton of task 2

---
+++ 04_PublicRepositoryEvents/Simplified.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+
+class App(Application):
+    def createWidgets(self):
+        self.message = "Congratulations!\nYou've found a sercet level!"
+        self.F1(tk.LabelFrame, "1:0", text="Frame 1")
+        self.F1.B1(tk.Button, "0:0/NW", text="1")
+        self.F1.B2(tk.Button, "0:1/NE", text="2")
+        self.F1.B3(tk.Button, "1:0+1/SEW", text="3")
+        self.F2(tk.LabelFrame, "1:1", text="Frame 2")
+        self.F2.B1(tk.Button, "0:0/N", text="4")
+        self.F2.B2(tk.Button, "0+1:1/SEN", text="5")
+        self.F2.B3(tk.Button, "1:0/S", text="6")
+        self.Q(tk.Button, "2.0:1.2/SE", text="Quit", command=self.quit)
+        self.F1.B3.bind("<Any-Key>", lambda event: showinfo(self.message.split()[0], self.message))
+
+
+if __name__ == '__main__':
+    app = App(title="Sample application")
+    app.mainloop()
