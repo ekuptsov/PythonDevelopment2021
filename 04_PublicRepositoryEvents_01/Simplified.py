1b629476c930926bb75548cc75d1d6ae3b706771
ekuptsov <s02150138@stud.cs.msu.ru>
2021-03-25 23:07:50 +0300

    Selfbuilding was separated in mixin class

---
+++ 04_PublicRepositoryEvents_01/Simplified.py
@@ -0,0 +1,95 @@
+import tkinter as tk
+from tkinter import messagebox
+import re
+
+
+class BuilderMixin():
+
+    GEOM_RE = re.compile(
+        r"(?P<row>\d+)"
+        r"(?:\.(?P<rowweight>\d+))?"
+        r"(?P<height>\+\d+)?"
+        ":"
+        r"(?P<column>\d+)"
+        r"(?:\.(?P<colweight>\d+))?"
+        r"(?P<width>\+\d+)?"
+        r"(?:/(?P<gravity>[NSWE]{1,4}))?"
+    )
+
+    def __init__(self, master, geometry, **options):
+        super().__init__(master, **options)
+        self.apply_geometry(**geometry)
+
+    def apply_geometry(self, row, column, rowweight=1, height=0,
+                       colweight=1, width=0, gravity='NSWE'):
+        row = int(row)
+        column = int(column)
+        rowweight = int(rowweight)
+        height = int(height)
+        colweight = int(colweight)
+        width = int(width)
+        self.grid(row=row, rowspan=height+1)
+        self.master.rowconfigure(row, weight=rowweight)
+        self.grid(column=column, columnspan=width+1)
+        self.master.columnconfigure(column, weight=colweight)
+        self.grid(sticky=gravity)
+
+    def smart_builder(self, tkClass, geometry, **options):
+        ''' Build new widget on self '''
+        class WidgetBuilder(BuilderMixin, tkClass):
+            pass
+
+        # Parse geometry and instantiate widget
+        match = re.fullmatch(self.GEOM_RE, geometry)
+        if not match:
+            raise Exception('Wrong geometry options')
+
+        specified = \
+            {k: v for k, v in match.groupdict().items() if v is not None}
+        return WidgetBuilder(self, specified, **options)
+
+    def __getattr__(self, name):
+        def create_and_set_obj(*args, **kwargs):
+            setattr(self, name, self.smart_builder(*args, **kwargs))
+        return create_and_set_obj
+
+
+class Application(tk.Frame, BuilderMixin):
+
+    def __init__(self, master=None, title=None):
+        super().__init__(master)
+        if master:
+            self.master = master
+        self.master.title(title)
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        self.grid(sticky='NSWE')
+        self.createWidgets()
+
+    def createWidgets(self):
+        pass
+
+
+class App(Application):
+
+    def createWidgets(self):
+        self.msg = "Congratulations!\nYou've found a sercet level!"
+        self.F1(tk.LabelFrame, "1:0", text="Frame 1")
+        self.F1.B1(tk.Button, "0:0/NW", text="1")
+        self.F1.B2(tk.Button, "0:1/NE", text="2")
+        self.F1.B3(tk.Button, "1:0+1/SEW", text="3")
+        self.F2(tk.LabelFrame, "1:1", text="Frame 2")
+        self.F2.B1(tk.Button, "0:0/N", text="4")
+        self.F2.B2(tk.Button, "0+1:1/SEN", text="5")
+        self.F2.B3(tk.Button, "1:0/S", text="6")
+        self.Q(tk.Button, "2.0:1.2/SE", text="Quit", command=self.quit)
+        self.F1.B3.bind(
+            '<Any-Key>',
+            lambda event: messagebox.showinfo(self.msg.split()[0], self.msg)
+        )
+
+
+if __name__ == '__main__':
+    app = App(title="Sample application")
+    app.mainloop()
